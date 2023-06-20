1958dab72038e82c478fb9b2214cd043c3104d6e
Shriyadita <shriyaditabhilkar@gmail.com>
2023-06-20 15:25:52 -0500

    Initial Commit

---
+++ EL Nutritious/EL_NutritiousApp.swift
@@ -0,0 +1,20 @@
+//
+//  EL_NutritiousApp.swift
+//  EL Nutritious
+//
+//  Created by Nitin Bhilkar on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct EL_NutritiousApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
