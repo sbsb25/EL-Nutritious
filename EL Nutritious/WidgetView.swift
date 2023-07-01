fa3fa8cf5daa154a97c11f0e25c3e02cbec44cd5
Shriyadita <shriyaditabhilkar@gmail.com>
2023-06-30 22:12:52 -0500

    edits

--- EL Nutritious/WidgetView.swift
+++ EL Nutritious/WidgetView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct WidgetView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Create your own Widget!")
+                .font(Font.custom("OptimaDisplay-Roman", size: 50, relativeTo: .title))
+                .font(.system(size: 40))
+                .fontWeight(.black)
+                .foregroundColor(Color("coralDark"))
+                .padding()
+                .padding()
+            
+            Text("Customize your homescreen with our new products! Keep track of your wishlist in a stylish way!")
+                .font(Font.custom("kzidenzGroteskBQ-Bold", size: 18, relativeTo: .title))
+                .font(.system(size: 40))
+                .fontWeight(.black)
+                .foregroundColor(Color("coralDark"))
+                .padding()
+                .padding()
+            
+            NavigationLink(destination: HomePageView()) {
+                Text("Shop!")
+                    .tint(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color("coralLight"))
+                    .cornerRadius(12)
+            }
+            
+        }
     }
 }
 
